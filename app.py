4eb2909fb9ae67bc15690a40fc8f169229a929b9
Alisha Khan <alisha.khan.mridula@gmail.com>
2023-05-31 16:49:39 +0600

    JWT

--- app.py
+++ app.py
@@ -2,14 +2,16 @@ from flask import Flask, request, jsonify, make_response, render_template, sessi
 import jwt
 from datetime import datetime, timedelta
 from functools import wraps
-
+import traceback
 app = Flask(__name__)
 
 #Secrect Key
-#app.config['SECRET_kEY'] = '57de392eaf3c4c1fa6a6333ba95eb57f68' 
+app.config['SECRET_KEY'] = '57de392eaf3c4c1fa6a6333ba95eb57f68' 
 
-app.secret_key = '57de392eaf3c4c1fa6a6333ba95eb57f68'
+#app.secret_key = '57de392eaf3c4c1fa6a6333ba95eb57f68'
 
+
+#### Token generation ####
 #JWT store the token in client side
 def token_required(func):
     @wraps(func)
@@ -19,17 +21,20 @@ def token_required(func):
             
            return jsonify({'Alert!' : 'Token is missing!'})
         try:
-          payload = jwt.decode(token, app.config['SECRET_KEY'])
+          data = jwt.decode(token, app.config['SECRET_KEY'], algorithms="HS256" )
           
-        except:
+        except Exception as e:
+            traceback.print_exc()
             return jsonify({'Alert!': 'Invalid Token'})
         return func(*args, **kwargs)
     return decorated
+
+
         
 
 
 #If user is not logedin , user will be redirect to home page login form
-#Home Page
+###### Home Page/Login Page ######
 @app.route('/')
 def home():
     if not session.get('logged_in'):
@@ -45,7 +50,7 @@ def public():
     return  'For Public'
 
 #For authenticated users
-@app.route('/auth')
+@app.route('/pub', methods = ['GET'])
 @token_required 
 def auth():
     return 'JWT is verified.'  
@@ -56,12 +61,14 @@ def auth():
 def userLogin():
     if request.form['username'] and request.form['email'] and request.form['number'] and request.form['password'] == '23234':
         session['logged_in'] = True
-        token = jwt.encode({'user': request.form['username'],'expiration' : str(datetime.utcnow()+ timedelta(seconds=60))},
-        #app.config['SECRET_KEY'])
-        app.secret_key)
+        token = jwt.encode({'user': request.form['username'],'expiration' : str(datetime.utcnow()+ timedelta(seconds=50))},
+        app.config['SECRET_KEY'])
+        #app.secret_key)
         return jsonify({'token': token})   
+        #return "Success"
     else: 
-        return make_response('Verfication Failed', 403,{'WWW-Authenticate': 'Basic realm:Authentication Failed!!'}) 
+        #return make_response('Verfication Failed', 403,{'WWW-Authenticate': 'Basic realm:Authentication Failed!!'}) 
+        return "Not Success"
     
     
 
